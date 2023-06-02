7ec86ad85929d20f83caa54f8f27fa1a83ca8055
ontime consultants <ontime.c23@gmail.com>
2023-06-02 17:33:46 +0300

    added vercel.json

--- controllers/user.js
+++ controllers/user.js
@@ -13,7 +13,7 @@ export const signin = async (req,res) => {
         const existingUser = await User.findOne({ email });
         if(!existingUser) return res.status(404).json({ message: "User doesn't exist."});
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
-        if(!isPasswordCorrect) return res.status(400).json({message: " Invalid Credentials"});
+        if(!isPasswordCorrect) return res.status(400).json({ message: " Invalid Credentials"});
         const token = jwt.sign({ email: existingUser.email, id: existingUser._id, role: existingUser.role }, secret, {expiresIn: '1h'});
 
         res.status(200).json({ result: existingUser, token });
@@ -31,13 +31,13 @@ export const signup = async (req,res) => {
         if(existingUser) return res.status(400).json({ message: "User already exists."});
         const isMember = await PostMessage.findOne({ email });
         if( !isMember ) return res.status(400).json({ message: "Kindly join the sacco to be a member."});
-        if(password !== confirmPassword) return res.status(400).json({message: "Passwords do not match"});
+        if(password !== confirmPassword) return res.status(400).json({ message: "Passwords do not match"});
         const hashedPassword = await bcrypt.hash(password, 12);
     
         const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`, role: 'user' });
-        const token = jwt.sign({ email: result.email, id: result._id}, secret, {expiresIn: '1h'} );
+        const token = jwt.sign({ email: result.email, id: result._id }, secret, {expiresIn: '1h'} );
         res.status(200).json({ result, token });
     } catch (error) {      
-        res.status(500).json({message: "Something went wrong"});
+        res.status(500).json({ message: "Something went wrong" });
     }
 }
--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const app = express();
 
 dotenv.config();
 
-app.use(express.json({limit:"30mb",extended:true}));
-app.use(express.urlencoded({limit:"30mb",extended:true}));
+app.use(express.json({ limit:"30mb",extended:true}));
+app.use(express.urlencoded({ limit:"30mb",extended:true}));
 
 app.use(cors());
 
